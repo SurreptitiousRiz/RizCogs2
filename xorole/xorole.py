ddd5e63dd1896cc725c56f78fc12a2aa30876661
SurreptitiousRiz <lbpietersen@gmail.com>
2021-07-09 15:59:56 +0200

    Update xorole.py
    s

--- xorole/xorole.py
+++ xorole/xorole.py
@@ -96,7 +96,7 @@ class XORole(BaseCog):
     def save(self):
         try:
             with open(JSON, 'w') as outputfile:
-                json.dump(self.settings, outputfile)
+                json.dumps(self.settings, outputfile)
         except:
             print('xorole.json not saved')
 
